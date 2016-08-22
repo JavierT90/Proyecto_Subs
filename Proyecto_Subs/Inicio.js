ed5333177692fe844b2cf02916a24e82c9262db6
Ricky <rickyyancor190@gmail.com>
2016-08-22 17:52:20 -0600

    Prueba traduccion hola mundo

--- Proyecto_Subs/Inicio.js
+++ Proyecto_Subs/Inicio.js
@@ -1,14 +1,26 @@
-// include the http module
-var http = require('http');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+var exec = require('child_process').exec;
+var execSync = require('exec-sync');
 
-// create a webserver
-http.createServer(function (req, res) {
 
-    // respond to any incoming http request
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+server.listen(8080);
 
-}).listen(8080, '127.0.0.1');
-
-// log what that we started listening on localhost:1337
-console.log('Server running at 127.0.0.1:8080');
+app.get('/',
+    function(req,res)
+    {
+        res.sendFile("/home/ricky/proyectoanalisis/Proyecto_Subs/Proyecto_Subs/Inicio.html");
+    }
+);
+app.get('/traducir',
+  function sendResponse(req,res)
+  {
+    console.log(req.query.texto);
+    console.log(req.query.idiomaorigen);
+    console.log(req.query.idiomadestino);
+    res.status(200).send('hola mundo');
+  }
+);
--- Proyecto_Subs/test/TDD_Prueba1.js
+++ Proyecto_Subs/test/TDD_Prueba1.js
@@ -1,1 +1,18 @@
-Texto prueba1
+var expect = require('chai').expect;
+var request = require('superagent');
+
+describe('Pagina de traducciones', function() {
+	var baseUrl = 'http://localhost:8080'
+	describe('Cuando se solicite el sitio Traducir',function(){
+		it('debería de obtener una cadena con hola mundo',function(done){
+			//Función esperada para que se cumpla la prueba
+			request.get(baseUrl + '/traducir?texto=hello%20world&idiomaorigen=EN&idiomadestino=ESP').end(function assert(err,res){
+				expect(err).to.not.be.ok;
+				expect(res).to.have.property('status',200);
+        expect(res.text).to.equal('hola mundo');
+				done();
+			});
+
+		});
+	});
+});
