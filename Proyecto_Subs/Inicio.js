a8d2812e91cb492e5d58113f20c2fd9f6e905e31
Ricky <rickyyancor190@gmail.com>
2016-08-22 19:38:44 -0600

    pasada la prueba de traduccion en espera de mejorar tiempos de respuesta app microsoft

--- Proyecto_Subs/Inicio.js
+++ Proyecto_Subs/Inicio.js
@@ -5,9 +5,13 @@ var io = require('socket.io')(server);
 var fs = require('fs');
 var exec = require('child_process').exec;
 var execSync = require('exec-sync');
-
+var MsTranslator = require('mstranslator');
 
 server.listen(8080);
+var client = new MsTranslator({
+        client_id: "772661829323321212",
+        client_secret: "gepZTKt3yjyLCW0uqRkYOA1+BhEip/eKtaVNdo8n2Uk="
+    }, true);
 
 app.get('/',
     function(req,res)
@@ -15,12 +19,43 @@ app.get('/',
         res.sendFile("/home/ricky/proyectoanalisis/Proyecto_Subs/Proyecto_Subs/Inicio.html");
     }
 );
+
 app.get('/traducir',
   function sendResponse(req,res)
   {
-    console.log(req.query.texto);
-    console.log(req.query.idiomaorigen);
-    console.log(req.query.idiomadestino);
-    res.status(200).send('hola mundo');
+    var query={text:req.query.texto,
+                from:req.query.idiomaorigen,
+              to:req.query.idiomadestino}
+
+    Traducir(query,function(Respuesta){
+        res.status(200).send(Respuesta);
+    });
   }
 );
+
+function Traducir(query,callback)
+{
+  console.log("Funcion Traducir");
+
+    client.translate(query , function(err,data)
+    {
+      if(err)
+      {
+        console.log(err);
+        callback("nel chavo");
+      }
+      else
+      {
+        callback(data.toLowerCase());
+      }
+    });
+}
+
+function traduccion(texto , origen , destino)
+{
+  var params = {
+              text: texto,
+              from: origen,
+              to: destino
+          };
+}
--- Proyecto_Subs/test/TDD_Prueba1.js
+++ Proyecto_Subs/test/TDD_Prueba1.js
@@ -6,7 +6,7 @@ describe('Pagina de traducciones', function() {
 	describe('Cuando se solicite el sitio Traducir',function(){
 		it('debería de obtener una cadena con hola mundo',function(done){
 			//Función esperada para que se cumpla la prueba
-			request.get(baseUrl + '/traducir?texto=hello%20world&idiomaorigen=EN&idiomadestino=ESP').end(function assert(err,res){
+			request.get(baseUrl + '/traducir?texto=hello%20world&idiomaorigen=en&idiomadestino=es').end(function assert(err,res){
 				expect(err).to.not.be.ok;
 				expect(res).to.have.property('status',200);
         expect(res.text).to.equal('hola mundo');
